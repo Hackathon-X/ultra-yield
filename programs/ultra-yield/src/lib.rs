e66fec9eb5b7981e4a85058296540b5fa31629bb
Noah <upupqi.cs@gmail.com>
2024-03-31 14:42:35 +0800

    feat: update some solana program

--- programs/ultra-yield/src/lib.rs
+++ programs/ultra-yield/src/lib.rs
@@ -72,6 +72,32 @@ pub mod ultra_yield {
         token::transfer(cpi_ctx, amount)?;
         Ok(())
     }
+
+    // 假设的获取积分函数，返回一个结果类型，这里简化为 u64
+    // fn get_points_from_whales_market() -> Result<u64, ProgramError> {
+    //     // 假设这里通过某种方式获取了积分，比如调用另一个合约的函数
+    //     Ok(100) // 返回一个示例积分值
+    // }
+
+    // pub fn interact_with_kamino(ctx: Context<WithdrawSol>, amount: u64) -> Result<()> {
+    //     // 构造一个转账指令，将SOL从from_account转移到to_account
+    //     let transfer_instruction = system_instruction::transfer(
+    //         &ctx.accounts.from_account.key(),
+    //         &ctx.accounts.to_account.key(),
+    //         amount,
+    //     );
+    
+    //     // 调用系统程序执行转账操作
+    //     invoke(
+    //         &transfer_instruction,
+    //         &[
+    //             ctx.accounts.from_account.to_account_info(),
+    //             ctx.accounts.to_account.to_account_info(),
+    //         ],
+    //     )?;
+    
+    //     Ok(())
+    // }
 }
 
 #[derive(Accounts)]
@@ -124,14 +150,24 @@ pub struct TransferUltrasol<'info> {
     pub token_program: Program<'info, Token>,
 }
 
+// #[derive(Accounts)]
+// pub struct InteractWithWhalesMarket<'info> {
+//     // 定义交云市场和用户账户等
+// }
+
+// #[derive(Accounts)]
+// pub struct InteractWithWhalesMarket {
+//     // 定义交云市场和用户账户等
+// }
+
 #[account]
 pub struct PoolAccount {
-    pub owner: Pubkey, // 池子的所有者或管理员
-    pub ultrasol_mint: Pubkey, // ultrasol 代币的 mint 地址
-    pub total_sol_deposited: u64, // 池中总共存储的 SOL 数量
+    pub owner: Pubkey,              // 池子的所有者或管理员
+    pub ultrasol_mint: Pubkey,      // ultrasol 代币的 mint 地址
+    pub total_sol_deposited: u64,   // 池中总共存储的 SOL 数量
     pub total_ultrasol_minted: u64, // 总共铸造的 ultrasol 数量
-    pub kamino_pool_id: u64, // 与此池子相关联的 Kamino 池子的标识符（或地址）
+    pub kamino_pool_id: u64,        // 与此池子相关联的 Kamino 池子的标识符（或地址）
     pub last_update_timestamp: i64, // 最后一次更新池子状态的时间戳
     pub total_lptokens_received: u64, // 从 Kamino 池子接收的 LP 代币总量
-    // 根据需要添加的其他字段
+                                    // 根据需要添加的其他字段
 }
